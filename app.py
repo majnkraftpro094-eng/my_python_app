5f301a554a7adc401ef5c969fb7ab251671e50cd
Roman Buteec <majnkraftpro094@gmail.com>
2026-03-12 17:55:21 +0200

    Pure english code fix

--- app.py
+++ app.py
@@ -2,6 +2,4 @@ def add(a, b):
     return a + b
 
 if __name__ == '__main__':
-    x = int(input('Enter a: '))
-    y = int(input('Enter b: '))
-    print('Result:', add(x, y))
+    print(add(2, 2))
